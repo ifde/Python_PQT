a6653e6e4ce27b0949050e8751087b77ff349c71
ifde <ifde@yandex.ru>
2021-11-24 17:50:46 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('design.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.run)
+        # Обратите внимание: имя элемента такое же как в QTDesigner
+
+    def run(self):
+        self.update()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        qp.setBrush(QColor(255, 255, 0))
+        r1 = randint(10, 50)
+        r2 = randint(10, 50)
+        qp.drawEllipse(randint(50, 450), randint(50, 450), r1, r2)
+        qp.end()
+        # Имя элемента совпадает с objectName в QTDesigner
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
